6e850e66d04829e75e40cd3f9346f6941b2a2412
Victor Zhao <victorsss.orz@outlook.com>
2025-04-21 17:05:41 -0500

    update database

--- src/lib/sqlDatabase.ts
+++ src/lib/sqlDatabase.ts
@@ -1,23 +1,23 @@
 import mysql, { Connection } from "mysql2/promise";
 
-export async function connectToDatabase(): Promise<Connection> {
-  const connection = await mysql.createConnection({
-    host: "localhost", // Use service name defined in docker-compose.yml
-    user: "victorsss_orz", // MySQL username
-    // password: "password", // MySQL password
-    database: "riichi", // Database name
-  });
-
-  return connection;
-}
-
 // export async function connectToDatabase(): Promise<Connection> {
 //   const connection = await mysql.createConnection({
 //     host: "localhost", // Use service name defined in docker-compose.yml
-//     user: "uiucriichi_admin", // MySQL username
-//     password: "uiucriichi1326", // MySQL password
-//     database: "uiucriichi_data", // Database name
+//     user: "victorsss_orz", // MySQL username
+//     // password: "password", // MySQL password
+//     database: "riichi", // Database name
 //   });
 
 //   return connection;
 // }
+
+export async function connectToDatabase(): Promise<Connection> {
+  const connection = await mysql.createConnection({
+    host: "localhost", // Use service name defined in docker-compose.yml
+    user: "uiucriichi_admin", // MySQL username
+    password: "uiucriichi1326", // MySQL password
+    database: "uiucriichi_data", // Database name
+  });
+
+  return connection;
+}
